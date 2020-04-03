832b8c30ec5e4567b31de20234ecc79d83c9aea2
conbrio1 <asdafe45@gmail.com>
2020-04-03 23:11:21 +0900

    applied object concept and added colors.js

---
+++ colors.js
@@ -0,0 +1,35 @@
+var Body = {
+    setColor: function(color) {
+        document.querySelector("body").style.color = color;
+    },
+    setBackgroundColor: function(color) {
+        /* The Document method querySelector() returns the first Element('tag')
+        within the document that matches the specified selector, or group of selectors.
+        If no matches are found, null is returned. */
+        document.querySelector("body").style.backgroundColor = color;
+    }
+};
+var Links = {
+    setColor: function(color) {
+        /* The Document method querySelectorAll() returns a static (not live) NodeList
+        representing a list of the document's elements that match the specified group of selectors. */
+        document.querySelectorAll("a").forEach(element => {
+            element.style.color = color;
+        });
+    }
+};
+function nightDayHandler(self) {
+    if (self.value === "night") {
+        Body.setBackgroundColor("black");
+        Body.setColor("white");
+        self.value = "day";
+
+        Links.setColor("powderblue");
+    } else {
+        Body.setBackgroundColor("white");
+        Body.setColor("black");
+        self.value = "night";
+
+        Links.setColor("blue");
+    }
+}
